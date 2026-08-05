4a2747a2895e943095e57b0597e1e00f497684d8
artistic-programmer <artistic.7.programmer@gmail.com>
2026-08-05 17:48:57 +0530

    Added Dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -14,6 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.example.android"
         minSdk = 25
+        //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -53,4 +56,31 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Retrofit -
+    // Retrofit core library
+    implementation(libs.retrofit)
+
+    // Choose ONE converter factory depending on your serialization choice:
+    // Option A: Gson (Most common)
+    implementation(libs.converter.gson)
+
+    // Option B: Kotlinx Serialization (Modern standard for Kotlin)
+    implementation(libs.retrofit2.converter.kotlinx.serialization)
+
+    // Coil - Image Loading
+    implementation(libs.coil.compose)
+
+    // Hilt - Dependency Injection
+    implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
+
+    // ksp - annotation preprocessor
+    ksp(libs.androidx.room.compiler)
+
+    // Room DB
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
 }
